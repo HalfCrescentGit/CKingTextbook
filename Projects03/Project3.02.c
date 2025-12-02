81951be2cf19dd16b082032cb3681c48303dd066
Josh <joshray639@gmail.com>
2025-12-02 18:27:02 +0000

    Project 3.02

--- Projects03/Project3.02.c
+++ Projects03/Project3.02.c
@@ -11,7 +11,7 @@ int main() {
     printf("Enter purchase date (mm/dd/yyyy): ");
     scanf("%d/%d/%d", &m, &d, &y);
 
-    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n%d\t$ %.2f\t%.2d/%.2d/%4d\n", itemNo, price, m, d, y);
+    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n%d\t$ %.2f\t%.2d/%.2d/%.4d\n", itemNo, price, m, d, y);
 
     return 0;
 }
