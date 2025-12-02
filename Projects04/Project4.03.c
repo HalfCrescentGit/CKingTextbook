deaf15409865f0fe346ad7feca0f0a1aa78c3e1b
Josh <joshray639@gmail.com>
2025-12-02 19:28:51 +0000

    Project 4.03

---
+++ Projects04/Project4.03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int digit0, digit1, digit2;
+
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d", &digit2, &digit1, &digit0);
+
+    printf("The reversal is: %d%d%d", digit0, digit1, digit2);
+
+    return 0;
+}
