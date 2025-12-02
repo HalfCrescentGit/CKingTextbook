c07af26fd0e5bea9226ed1d80d87aca35355fec3
Josh <joshray639@gmail.com>
2025-12-02 19:37:07 +0000

    Project 4.04

--- Projects04/Project4.04.c
+++ Projects04/Project4.04.c
@@ -6,11 +6,11 @@ int main() {
     printf("Enter an integer between 0 and 32767: ");
     scanf("%d", &x);
 
-    oct0 = x % 8;
-    oct1 = (x / 8) % 8;
-    oct2 = (x / 64) % 8;
-    oct3 = (x / 512) % 8;
-    oct4 = (x / 4096) % 8;
+    oct0 = x % 8; x/= 8;
+    oct1 = x % 8; x/= 8;
+    oct2 = x % 8; x/= 8;
+    oct3 = x % 8; x/= 8;
+    oct4 = x % 8;
 
     printf("In octal, your number is: %d%d%d%d%d", oct4, oct3, oct2, oct1, oct0);
 
