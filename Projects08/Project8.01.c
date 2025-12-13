22bef96f91aef9b5c50398cb9ed5a50b0547c4e1
Josh <joshray639@gmail.com>
2025-12-13 04:50:07 +0000

    Project 8.01

---
+++ Projects08/Project8.01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool digit_seen[10] = {false}, digit_repeated[10] = {false}, any;
+    int digit, i;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0) {
+        digit = n % 10;
+        if (digit_seen[digit])
+            digit_repeated[digit] = true;
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+    for (i = 0; i < 10; i++)
+        if (digit_repeated[i])
+            any = true;
+
+    if (!any)
+        printf("No repeated digit\n");
+    else
+        printf("Repeated digit(s):");
+
+    for (i = 0; i < 10; i++)
+        if (digit_repeated[i] == true)
+            printf(" %d", i);
+
+    return 0;
+}
